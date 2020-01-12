476fd24c4c05c61a6a45e850ca30ad1aac66751a
Naeeb <md.naeeb@gmail.com>
2020-01-12 15:48:39 +0600

    changing footer address, and digest tabs rearrange in front page

--- wp-content/themes/bgmea/index-parts/part5.php
+++ wp-content/themes/bgmea/index-parts/part5.php
@@ -1,7 +1,7 @@
 <!-- omi's part -->
 <section class="o_media_section container">
     <div class="">
-    <h3 class="o_media_headline">Media & Events</h3>
+    <h3 class="o_media_headline">Media & events</h3>
     <div class="row">
         <div class="col-lg-9">
             <?php
@@ -39,16 +39,17 @@
         </div>
         <div class="col-lg-3 o_media_section_right_sidebar">
         <div class="o_president_box_wrap">
-            <a href="#"><img class="o_media_section_president_img" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/12/monthly-digest-min-2.png" alt="image not found">
-            <p class="o_president_statement_link">Monthly Digest</p></a>
+            <a href="#"><img class="o_media_section_president_img o_media_section_our_team_img" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/12/monthly-digest-min-2.png" alt="image not found">
+            <p class="o_president_statement_link">Weekly digest</p></a>
          </div>
-            <div class="o_president_box_wrap">
-            <a href="<?php echo get_home_url(); ?>/media-events/upcoming-events/"><img class="o_media_section_president_img o_media_section_our_team_img" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/11/BGMEA_2.jpg" alt="image not found">
-                <p class="o_president_statement_link">Upcoming Events</p></a>
-            </div>
-            <div class="o_president_box_wrap o_election_box_wrap">
-                <img class="o_media_section_president_img o_media_section_election_img" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/11/daily_sun_BGMEA-Election-2019-2021-jp_picture.jpg" alt="image not found">
-            </div>
+        <div class="o_president_box_wrap">
+            <a href="<?php echo get_home_url(); ?>/media-events/upcoming-events/"><img class="o_media_section_president_img o_media_section_our_team_img" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/12/monthly-digest-min-2.png" alt="image not found">
+            <p class="o_president_statement_link">Monthly digest</p></a>
+        </div>
+        <div class="o_president_box_wrap">
+            <a href="#"><img class="o_media_section_president_img o_media_section_our_team_img" src="<?php echo get_home_url(); ?>/wp-content/uploads/2019/12/monthly-digest-min-2.png" alt="image not found">
+            <p class="o_president_statement_link">Projects</p></a>
+        </div>
         </div>
     </div>
     </div>
